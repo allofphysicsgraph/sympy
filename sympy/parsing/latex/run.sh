5c577c0480cf283bf531751054c2e632f7d5f552
mike <mike@mathrules.us>
2022-12-21 00:09:37 +0000

    building up a list of tokens seen in expressions

--- sympy/parsing/latex/run.sh
+++ sympy/parsing/latex/run.sh
@@ -1,7 +1,9 @@
+#./run.sh 100|awk -F, '{print $3}'|tr '\n' ' '|tr -d "<>'"|sed -f sed_file 
+
 CLASSPATH=".:/usr/local/lib/antlr-4.11.1-complete.jar:$CLASSPATH"
 
 java -Xmx500M -cp "/usr/local/lib/antlr-4.11.1-complete.jar:$CLASSPATH" org.antlr.v4.Tool CommonTokensLexer.g4
-javac *.java
 
+javac *.java
 
 java -Xmx500M -cp "/usr/local/lib/antlr-4.11.1-complete.jar:$CLASSPATH" org.antlr.v4.gui.TestRig CommonTokens tokens -tokens "$1" 
